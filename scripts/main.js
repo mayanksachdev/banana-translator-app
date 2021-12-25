6172a8e04423b5b5a21a78cf41e9ee73815ebd84
mayanksachdev <mayank.sdv@gmail.com>
2021-12-25 19:43:07 +0530

    feat: added a feature that converts plain english text into banana language

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+let myButton = document.querySelector('button')
+let myTextArea = document.querySelector('textarea')
+let myOutputDiv = document.querySelector('.div-output')
+
+let serverUrl = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json'
+
+function addUrlParameter(text) {
+    let serverUrlParameter = serverUrl + '?' + 'text=' + text;
+    return serverUrlParameter;
+}
+
+
+function fetchUrl(parameterValue) {
+
+    fetch(addUrlParameter(parameterValue))
+        .then(response => response.json())
+        .then(jsonFile => {
+            let outputValue = jsonFile.contents.translated;
+            myOutputDiv.innerText = outputValue
+        })
+        .catch(error => {
+                console.log('error has occured: ' + error)
+                alert('there is some problem with the server. please, come back later.')
+
+            })
+        }
+
+
+    myButton.addEventListener('click', function () {
+        console.log('clicked');
+        let userInput = myTextArea.value;
+        if (!userInput) {
+            alert('please enter a valid text!')
+        } else {
+            fetchUrl(userInput)
+        }
+
+    })
